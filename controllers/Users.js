f05f67f8f16fcfc0033c36a83db5bbfa88536297
papaganesha <joaopedro.ads2021@gmail.com>
2022-10-16 02:39:43 -0300

    adicionando storage e upload de imagens do usuario/arrumando updateUserInfo

--- controllers/Users.js
+++ controllers/Users.js
@@ -153,10 +153,10 @@ controller.getUsers = async (req, res) => {
 
 controller.updateUserInfo = async (req, res, next) => {
   const id = req.userId
-  console.log("req => ",req)
+
   console.log("req.body => ",req.body)
   console.log("req.file => ",req.file)
-  
+
   const result = await cloudinary.uploader.upload(req.file.path, {
     public_id: `${id}_profile`,
     width: 500,
