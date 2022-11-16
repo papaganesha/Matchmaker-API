a3a85970c304567022e29f1c67df64b07fda87a3
papaganesha <joaopedro.ads2021@gmail.com>
2022-11-16 20:40:16 -0300

    ajustes finais

--- middlewares/AuthenticationHandler.js
+++ middlewares/AuthenticationHandler.js
@@ -1,37 +1,6 @@
 const jwt = require('jsonwebtoken')
-const CometChat = require('@cometchat-pro/chat')
 
 
-
-const initializeCometchat = (UID) => {
-    //VARIAVEIS AMBIENTE DA APLICAÇÃO
-    const appID = '22363314d6b05de2';
-    const region = 'us';
-    const authKey = 'cefa05028acbf59fc97a08e61ad0f14765251514';
-  
-  
-  
-    //PARAMETROS DE CONFIGURAÇÃO PRÉDEFINIDOS
-    const appSetting = new CometChat.AppSettingsBuilder()
-        .subscribePresenceForAllUsers()
-        .setRegion(region)
-        .build()
-  
-    //INICIALIZANDO COMETCHAT COM OS PARAMETROS 
-    CometChat.init(appID, appSetting).then(
-        () => {
-            console.log('Initialization completed successfully');
-            // You can now call login function.
-        },
-        (error) => {
-            console.log('Initialization failed with error:', error);
-            // Check the reason for error and take appropriate action.
-        },
-    );
-  
-    loginCometchat(UID, authKey)
-  
-  }
 
 const isAuth = (req, res, next) => {
     //req.userId = null
@@ -50,7 +19,6 @@ const isAuth = (req, res, next) => {
         res.status(401).json({ message: 'unauthorized' });
     } else {
         req.userId = decodedToken.id
-        initializeCometchat(decodedToken.id)
         next();
     };
 };
